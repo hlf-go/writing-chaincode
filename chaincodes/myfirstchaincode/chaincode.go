3137bd0d3c5ec8d5077c58f446881188a6045d88
Paul Sitoh <paul.sitoh@uk.ibm.com>
2017-08-12 21:21:36 +0100

    Updated fabric support
    modified:   chaincodes/myfirstchaincode/chaincode.go
    modified:   fabric/docker-compose.yml
    modified:   fabric/scripts/myfirstchaincode.sh

--- chaincodes/myfirstchaincode/chaincode.go
+++ chaincodes/myfirstchaincode/chaincode.go
@@ -12,12 +12,14 @@ type SimpleChaincode struct{}
 
 // Init to initiate the SimpleChaincode class
 func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface) pb.Response {
-	return pb.Response{Status: 0, Message: "nothing", Payload: []byte{0, 0}}
+	fmt.Println("Hello Init")
+	return shim.Success([]byte("Init called"))
 }
 
 // Invoke a method specified in the SimpleChaincode class
 func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface) pb.Response {
-	return pb.Response{Status: 0, Message: "nothing", Payload: []byte{0, 0}}
+	fmt.Println("Hello Invoke")
+	return shim.Success([]byte("Invoke"))
 }
 
 func main() {
--- fabric/scripts/myfirstchaincode.sh
+++ fabric/scripts/myfirstchaincode.sh
@@ -1,34 +1,41 @@
 #!/bin/bash
 
 CHANNELNAME="mychannel"
-CHAINCODEID="mfcc"
+CHAINCODEID="mycc"
 
 echo
 echo "========================================================="
-echo " Installing, instantiating and invoking myfirstchaincode "
+echo "           START - myfirstchaincode                      "
 echo "========================================================="
 
 echo
 echo "-- Creating my myfirstchannel --"
 echo
-peer channel create -o orderer.example.com:7050 -c $CHANNELNAME -f ./channel-artifacts/channel.tx 2>&1  &> ./logs/myfirstchaincode.log
+peer channel create -o orderer.example.com:7050 -c $CHANNELNAME -f ./channel-artifacts/channel.tx   
 
 echo
 echo "-- Join channel --"
 echo
-peer channel join -b $CHANNELNAME.block 2>&1  &> ./logs/myfirstchaincode.log
+peer channel join -b $CHANNELNAME.block 
 
 echo
 echo "-- Installing chaincode --"
 echo
-peer chaincode install -n $CHAINCODEID -v 1.0 -p github.com/hyperledger/fabric/examples/chaincode/go/myfirstchaincode 2>&1  &> ./logs/myfirstchaincode.log
+peer chaincode install -n $CHAINCODEID -v 1.0 -p github.com/hyperledger/fabric/examples/chaincode/go/myfirstchaincode
 
 echo
 echo "-- Instantiating chaincode --"
 echo
-peer chaincode instantiate -o orderer.example.com:7050 -C $CHANNELNAME -n $CHAINCODEID -v 1.0 -c '{"Args":["Init","a","100","b","200"]}' -P "OR ('Org1MSP.member')" 2>&1  &> ./logs/myfirstchaincode.log
+peer chaincode instantiate -o orderer.example.com:7050 -C $CHANNELNAME -n $CHAINCODEID -v 1.0 -c '{"Args":["Init","a","100","b","200"]}' -P "OR ('Org1MSP.member')" 
+
+sleep 10
 
 echo
 echo "-- Invoking chaincode --"
 echo
-peer chaincode invoke -o orderer.example.com:7050 -C $CHANNELNAME -n $CHAINCODEID -c '{"Args":["invoke","a","b","10"]}' 2>&1  &> ./logs/myfirstchaincode.log
+peer chaincode invoke -o orderer.example.com:7050 -C $CHANNELNAME -n $CHAINCODEID -c '{"Args":["invoke","a","b","10"]}'
+
+echo
+echo "========================================================="
+echo "              END - myfirstchaincode                     "
+echo "========================================================="
